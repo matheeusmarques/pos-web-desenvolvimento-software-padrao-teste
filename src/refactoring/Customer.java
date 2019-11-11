468a593d06a13a44c8ab334956137b233d649e86
Aluno <matheeus.marques@live.com>
2019-11-11 20:01:00 -0300

    Refactoring 3: Move Method

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -31,28 +31,6 @@ public class Customer {
         return _name;
     }
 
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2) {
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
-
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
@@ -62,8 +40,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            thisAmount = amountFor(each);
-
+            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -82,5 +59,9 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints)
                 + " frequent renter points";
         return result;
+    }
+
+    private double amountFor(Rental aRental) {
+        return aRental.getCharge();  // agora apenas delega chamada para método movido
     }
 }
